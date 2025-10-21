e007904dbf3c4c3b2648f31d99791ef0b93d709c
bibitchhetri <kunwar.bibit7@gmail.com>
2025-10-21 10:49:11 +0545

    added: ivy

--- config.el
+++ config.el
@@ -48,11 +48,20 @@
 (my/leader-keys
  ;; Buffer
  "b"  '(:ignore t :which-key "buffer")
- "b b" '(switch-to-buffer :which-key "Switch buffer")
+ "b b" '(ivy-switch-buffer :which-key "Switch buffer")
  "b k" '(kill-this-buffer :which-key "Kill buffer")
  "b n" '(next-buffer :which-key "Next buffer")
  "b p" '(previous-buffer :which-key "Previous buffer")
  "b r" '(revert-buffer :which-key "Reload buffer")
+ "b B" '(ivy-switch-buffer-other-window :which-key "Switch buffer other window")
+
+ ;; File
+ "f" '(:ignore t :which-key "file")
+ "f f" '(counsel-find-file :which-key "Find file")
+ "f r" '(counsel-recentf :which-key "Recent files")
+ "f d" '(counsel-dired :which-key "Open dired")
+ "f p" '(sudo-edit-find-file :which-key "Sudo find file")
+ "f P" '(sudo-edit :which-key "Sudo edit file")
 
  ;; Window
  "w"  '(:ignore t :which-key "window")
@@ -60,6 +69,12 @@
  "w h" '(split-window-below :which-key "Split horizontal")
  "w c" '(delete-window :which-key "Delete window")
  "w o" '(delete-other-windows :which-key "Maximize window")
+
+ ;; Search
+ "s" '(:ignore t :which-key "search")
+ "s f" '(counsel-rg :which-key "Search in files")
+ "s b" '(counsel-switch-buffer :which-key "Search buffers")
+ "s m" '(counsel-imenu :which-key "Search in buffer")
 
  ;; Evaluate
  "e" '(:ignore t :which-key "Evaluate")    
@@ -71,14 +86,19 @@
 
  ;; Help
  "h" '(:ignore t :which-key "Help")
- "h f" '(describe-function :which-key "Describe function")
- "h v" '(describe-variable :which-key "Describe variable")
+ "h f" '(counsel-describe-function :which-key "Describe function")
+ "h v" '(counsel-describe-variable :which-key "Describe variable")
  "h r r" (lambda () (interactive) (load-file "~/.emacs.d/init.el")) :which-key "Reload emacs config"
 
  ;; Toggle
  "t" '(:ignore t :which-key "Toggle")
  "t l" '(display-line-numbers-mode :which-key "Toggle line numbers")
- "t t" '(visual-line-mode :which-key "Toggle truncated lines"))
+ "t t" '(visual-line-mode :which-key "Toggle truncated lines")
+
+ ;; Applications
+ "a" '(:ignore t :which-key "applications")
+ "a a" '(counsel-M-x :which-key "M-x")
+ "a r" '(ivy-resume :which-key "Resume last search"))
 
 (add-hook 'emacs-startup-hook
           (lambda ()
@@ -108,6 +128,21 @@
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
 (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; macOS window styling
+(when (eq system-type 'darwin)
+  ;; Remove window decorations and add rounded corners
+  (add-to-list 'default-frame-alist '(undecorated-round . t))
+  ;; Apply rounded corners to existing frame
+  (set-frame-parameter (selected-frame) 'undecorated-round t))
+
+(global-display-line-numbers-mode 1)
+(global-visual-line-mode 1)
+(setq display-line-numbers-type 'relative)
 
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
 (setq version-control t
@@ -151,7 +186,64 @@
       which-key-side-window-max-height 0.25
       which-key-idle-delay 0.8
       which-key-max-description-length 25
-      which-key-allow-imprecise-window-fit t
+      which-key-allow-imprecise-window-fit nil
       which-key-separator "   ")
+
+(straight-use-package 'ivy)
+(straight-use-package 'counsel)
+(straight-use-package 'ivy-rich)
+
+;; Ivy configuration
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+(setq enable-recursive-minibuffers t)
+
+;; Enable Ivy mode
+(ivy-mode 1)
+
+;; Counsel configuration
+(counsel-mode 1)
+
+;; Ivy-rich configuration
+(ivy-rich-mode 1)
+
+;; Optional: all-the-icons-ivy-rich (if available)
+(when (require 'all-the-icons-ivy-rich nil t)
+  (all-the-icons-ivy-rich-mode 1))
+
+;; Ivy-rich customizations
+(setq ivy-virtual-abbreviate 'full
+      ivy-rich-switch-buffer-align-virtual-buffer t
+      ivy-rich-path-style 'abbrev)
+
+(ivy-set-display-transformer 'ivy-switch-buffer
+                             'ivy-rich-switch-buffer-transformer)
+
+;; Additional Ivy keybindings (Doom Emacs style)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "C-x B") 'ivy-switch-buffer-other-window)
+(global-set-key (kbd "C-s") 'counsel-grep-or-swiper)
+(global-set-key (kbd "C-r") 'counsel-grep-or-swiper-backward)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;; Set transparency and blur for new frames (GUI mode only)
+(when (display-graphic-p)
+  ;; Transparency settings
+  (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+  ;; Apply transparency to existing frame
+  (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+  
+  ;; Blur effect for macOS
+  (when (eq system-type 'darwin)
+    (add-to-list 'default-frame-alist '(ns-appearance . dark))
+    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+    ;; Enable blur effect
+    (add-to-list 'default-frame-alist '(ns-background-blur . t))
+    ;; Apply blur to existing frame
+    (set-frame-parameter (selected-frame) 'ns-background-blur t)))
 
 (load-theme 'tango-dark t)
