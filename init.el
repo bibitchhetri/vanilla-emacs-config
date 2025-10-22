d240442ddf36ca3e2386bebfb8b1fe55cc7c58de
bibitchhetri <kunwar.bibit7@gmail.com>
2025-10-22 18:51:20 +0545

    info: custom generated safe themes

--- init.el
+++ init.el
@@ -1,2 +1,17 @@
 (org-babel-load-file
  (expand-file-name "config.org" (file-name-directory load-file-name)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd"
+     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
+     default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
